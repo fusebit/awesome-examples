c24c4b2655c827c383d3b4e657fee50de7001e09
Matthew Zhao <matthew@fusebit.io>
2021-12-07 12:04:40 -0800

    Add Linear Slack Integration

---
+++ linear-slack/linear-slack-congratulation/integration.js
@@ -0,0 +1,18 @@
+const { Integration } = require('@fusebit-int/framework');
+
+const integration = new Integration();
+
+const slackConnector = 'slack-e2e-linear';
+
+integration.event.on('/:componentName/webhook/:eventtype', async (ctx) => {
+  if (ctx.params.eventtype === 'Issue.update' && ctx.req.body.data.data.state.name === 'Done') {
+    const slackClient = await integration.service.getSdk(ctx, slackConnector, ctx.req.body.installIds[0])
+    const result = await slackClient.chat.postMessage({
+    text: `Congratulations, ${ctx.req.body.data.data.assignee.name} for completing a new issue!`,
+    channel: 'bot-linear',
+  });
+
+  }
+});
+
+module.exports = integration;
