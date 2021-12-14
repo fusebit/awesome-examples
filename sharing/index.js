56e362f3b6207d5f104f4e50bee695d936aae417
Benn Bollay <bb.github@magitech.org>
2021-12-14 14:55:42 -0800

    .

--- sharing/index.js
+++ sharing/index.js
@@ -1,17 +1,16 @@
-/**
- * @param ctx {FusebitContext}
- */
+const fusebitPortal = "https://manage.fusebit.io/callback";
+
 module.exports = async (ctx) => {
-  // const url = new URL("https://manage.fusebit.io/callback");
-  const url = new URL("http://localhost:3000/callback");
+  const url = new URL(fusebitPortal);
+
+  const interationPath = `/account/${ctx.params.accountId}/subscription/${ctx.params.subscriptionId}/integration/${ctx.query.integrationId}/edit`;
+
   url.searchParams.set("silentAuth", false);
-  url.searchParams.set(
-    "requestedPath",
-    "/account/acc-6d026dbd88af4eaf/subscription/sub-025ba376ff9d4ec5/integration/linear-growth/edit"
-  );
+  url.searchParams.set("requestedPath", integrationPath);
   url.searchParams.set("scope", "openid profile email");
   url.searchParams.set("expires_in", "86400");
   url.searchParams.set("token_type", "Bearer");
   url.hash = `#access_token=${ctx.fusebit.functionAccessToken}`;
-  return { body: url.toString() };
+
+  return { status: 302, headers: { location: url.toString() } };
 };
