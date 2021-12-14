646ef8bd779a9de6131d340299661293696a6040
Benn Bollay <bb.github@magitech.org>
2021-12-14 14:56:36 -0800

    Post prettier

--- sharing/index.js
+++ sharing/index.js
@@ -1,15 +1,15 @@
-const fusebitPortal = "https://manage.fusebit.io/callback";
+const fusebitPortal = 'https://manage.fusebit.io/callback';
 
 module.exports = async (ctx) => {
   const url = new URL(fusebitPortal);
 
   const interationPath = `/account/${ctx.params.accountId}/subscription/${ctx.params.subscriptionId}/integration/${ctx.query.integrationId}/edit`;
 
-  url.searchParams.set("silentAuth", false);
-  url.searchParams.set("requestedPath", integrationPath);
-  url.searchParams.set("scope", "openid profile email");
-  url.searchParams.set("expires_in", "86400");
-  url.searchParams.set("token_type", "Bearer");
+  url.searchParams.set('silentAuth', false);
+  url.searchParams.set('requestedPath', integrationPath);
+  url.searchParams.set('scope', 'openid profile email');
+  url.searchParams.set('expires_in', '86400');
+  url.searchParams.set('token_type', 'Bearer');
   url.hash = `#access_token=${ctx.fusebit.functionAccessToken}`;
 
   return { status: 302, headers: { location: url.toString() } };
